12a7bebba12af140f01f68467756e4a62f50ce8b
timashabu <101049044+timashabu@users.noreply.github.com>
2022-03-21 02:06:39 +0000

    Update _helper.js

--- assets/plugins/_helper.js
+++ assets/plugins/_helper.js
@@ -15,18 +15,18 @@ Copyright (C) 2021 Black Amda.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Coded by : Sasmitha (Sinhalaya Creator)
+Coded by : M.R TIMA (Timasha)
 */
 
-const QueenAmdi = require('queenamdi-public');
-const Amdi = QueenAmdi.events
-const _amdi = QueenAmdi.panel
+const BlackAlpha = require('blackalpha-public');
+const ALPHA = BlackAlpha.events
+const _alpha = BlackAlpha.tima
 
 const Language = require('../language');
 const Lang = Language.getString('helper');
 
-Amdi.operate({ pattern: 'helper ?(.*)', fromMe: true, desc: Lang.DESC, deleteCommand: false, dontAddCommandList: true}, (async (amdiMSG, input) => {
-    await QueenAmdi.amdi_setup()
+Alpha.operate({ pattern: 'helper ?(.*)', fromMe: true, desc: Lang.DESC, deleteCommand: false, dontAddCommandList: true}, (async (alphaMSG, input) => {
+    await BlackAlpha.alpha_setup()
     const help_CMD = input[1]
-    await _amdi.helper( amdiMSG, help_CMD, Lang )
+    await _alpha.helper( alphaMSG, help_CMD, Lang )
 }));
