a9d0e4efb9e7afc984d47e5d3f7a28984be23ed8
timashabu <101049044+timashabu@users.noreply.github.com>
2022-03-24 14:13:23 +0000

    Update and rename QA-SQ.js to BA-SQ.js

---
+++ BA-SQ.js
@@ -0,0 +1,5 @@
+module.exports = {
+    BLACK_ALPHA_OWNER: '94773585511', // M.R TIMA - Black Alpha Owner
+    BLACK_ALPHA_TEAM: '94715264791,947,94774976567,94766426385,94711870791', // Team - BLACK ALPHA TEAM
+    SEW_QUEEN_OWNER: '94785435462' // Ravindu Manoj - Sew Queen Owner --> For The Friendship 
+}; 
